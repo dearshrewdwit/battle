c5e7f1bfa892fb2010831f08882d42883da8d712
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-28 14:25:37 +0000

    added attack

--- spec/features/user_name_spec.rb
+++ spec/features/user_name_spec.rb
@@ -1,6 +1,6 @@
 
 feature 'Enter names' do
-  
+
   before do
     sign_in_and_play
   end
@@ -11,6 +11,15 @@ feature 'Enter names' do
 
   scenario 'see hit points' do
     expect(page).to have_content 'Octavian: 100hp'
+  end
+
+  scenario 'attack player 2' do
+    click_button('Attack!')
+    expect(page).to have_content("Edward attacked Octavian!")
+  end
+
+  scenario 'get attack confirmation' do
+
   end
 
 end
