f7031371040a4a051bf0e2639aac229b12ed3c62
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-28 13:51:24 +0000

    added hitpoints

--- spec/features/user_name_spec.rb
+++ spec/features/user_name_spec.rb
@@ -1,10 +1,19 @@
 
 feature 'Enter names' do
-  scenario 'submitting names' do
+
+  before do
     visit('/')
     fill_in :player_1, with: 'Edward'
     fill_in :player_2, with: 'Octavian'
     click_button 'Submit'
+  end
+
+  scenario 'submitting names' do
     expect(page).to have_content 'Edward vs Octavian'
   end
+
+  scenario 'see hit points' do
+    expect(page).to have_content 'Octavian: 100hp'
+  end
+
 end
